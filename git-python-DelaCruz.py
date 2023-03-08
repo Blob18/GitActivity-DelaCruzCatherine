ecfa7789226bcf405ad257a1f5f025137c9ffe89
DelaCruzCatherine <catherinedelacruz833@gmail.com>
2023-03-08 20:55:30 +0800

    My Hobbies Added

--- git-python-DelaCruz.py
+++ git-python-DelaCruz.py
@@ -1,13 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  8 20:04:10 2023
-
-@author: Catherine
-"""
-
 print("Name:Catherine Dela Cruz")
 print("Adress:Tanza, Boac, Marinduque")
 print("Birthday:February 23 2003")
 print("Course:BACHELOR OF SCIENCE IN INFORMATION Technology")
 print("Year:2nd Year")
 print("BSI/T 2C")
+print("Animal Keeping")
+print("Exploring the Internet")
+print ("Playing Online Games")
+print("Taking Photos with Nature")
+print ("Traveling")
