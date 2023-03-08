2e000272be3b71985b6048bca90be4dd3210f439
DelaCruzCatherine <catherinedelacruz833@gmail.com>
2023-03-08 20:09:52 +0800

    My Personal Info Added

---
+++ git-python-DelaCruz.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  8 20:04:10 2023
+
+@author: Catherine
+"""
+
+print("Name:Catherine Dela Cruz")
+print("Adress:Tanza, Boac, Marinduque")
+print("Birthday:February 23 2003")
