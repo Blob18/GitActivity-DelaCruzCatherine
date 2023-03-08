3faf9808c2dcca45f5a32dd819468ff25045b23f
DelaCruzCatherine <catherinedelacruz833@gmail.com>
2023-03-08 20:18:50 +0800

    My Educational Background Added

--- git-python-DelaCruz.py
+++ git-python-DelaCruz.py
@@ -8,3 +8,6 @@ Created on Wed Mar  8 20:04:10 2023
 print("Name:Catherine Dela Cruz")
 print("Adress:Tanza, Boac, Marinduque")
 print("Birthday:February 23 2003")
+print("Course:BACHELOR OF SCIENCE IN INFORMATION Technology")
+print("Year:2nd Year")
+print("BSI/T 2C")
